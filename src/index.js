6e8afe7b49246860075c46d1436b75d92cb138f9
himoura32 <shawn.ahearn@outlook.com>
2022-05-06 10:09:22 -0400

    coded props and styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDom.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// ReactDom.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<App />);
